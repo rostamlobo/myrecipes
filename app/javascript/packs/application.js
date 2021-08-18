df2b851ecd70bc94559d894544c28011b531a61e
rostamlobo <rostamlobolobo2@gmail.com>
2021-08-18 11:21:59 -0300

    updating database

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,16 +3,9 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
-import 'bootstrap'
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require bootstrap-sprockets
+//= require_tree .
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
